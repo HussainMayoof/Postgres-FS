ab94a8afec36faa90537b3e1f4cf905542a376f7
Hussain <113285362+HussainMayoof@users.noreply.github.com>
2026-05-16 21:45:51 +0300

    Fixed model syncing issue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { PORT } from './src/util/config.js';
 import app from './src/app.js';
 import { connectToDatabase } from './src/util/db.js';
+import { syncModels } from './src/models/index.js';
 
 const start = async () => {
     await connectToDatabase();
+    await syncModels();
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
--- src/models/index.js
+++ src/models/index.js
@@ -2,20 +2,19 @@ import Note from './Note.js';
 import Blog from './Blog.js';
 import User from './User.js';
 
-User.sync();
-Note.sync();
-Blog.sync();
-
 User.hasMany(Note);
 User.hasMany(Blog);
 
 Note.belongsTo(User);
 Blog.belongsTo(User);
 
-Note.sync({ alter: true });
-Blog.sync({ alter: true });
-User.sync({ alter: true });
+const syncModels = async () => {
+    await User.sync({ alter: true });
+    await Note.sync({ alter: true });
+    await Blog.sync({ alter: true });
+};
 
 export { Note };
 export { Blog };
 export { User };
+export { syncModels };
